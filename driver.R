01d95ee8c8549eee52eab93c6731f262ab68a3f6
barbarian1803 <bharata180@gmail.com>
2016-07-26 17:30:18 +0900

    Create driver to check functionality

--- driver.R
+++ driver.R
@@ -19,3 +19,19 @@ allResult <- list()
 for(n in colnames(canonPathway)){
   allResult[[n]] <- ProcessTF(n,canonPathway)
 }
+
+
+
+
+pi3Pathway <- getPathwayRelationshipTable("hsa04151")
+newpi3Pathway <- getEnsemblBasedRelTable(pi3Pathway)
+pi3adj <- createAdjacencyMatrix(newpi3Pathway,TRUE)
+separatedpi3adj <- separateAdjMatrix(pi3adj)
+for(i in (1:length(separatedpi3adj))){
+  print(nrow(separatedpi3adj[[i]]))
+}
+
+allResultPI3 <- list()
+for(n in colnames(separatedpi3adj[[1]])){
+  allResultPI3[[n]] <- ProcessTF(n,separatedpi3adj[[1]])
+}
