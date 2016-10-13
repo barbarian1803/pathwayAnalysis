e0ea3717e735d9b11c94dbe15ca45da981055333
barbarian1803 <bharata180@gmail.com>
2016-10-13 10:52:09 +0900

    comments for driver

--- driver.R
+++ driver.R
@@ -9,15 +9,26 @@ load("data/DESeq2resultBDC.dat")
 wntPathway <- getPathwayRelationshipTable("hsa04310")
 newWNTPathway <- getEnsemblBasedRelTable(wntPathway)
 WNTadj <- createAdjacencyMatrix(newWNTPathway,TRUE)
-separatedWNTadj <- separateAdjMatrix(WNTadj)
-canonPathway <- separatedWNTadj[[3]]
+separatedWNTadj <- separateAdjMatrix(WNTadj) #KEGG network contain subnetwork, separate to individual network
+canonPathway <- separatedWNTadj[[3]] #manually choose the canon WNT pathway
 
 logFCData <- FC_BDC_Normal
+
 #check for one gene, LEF1
 #ProcessTF is the function that calculate the regression between target and TFs gene
 convertGeneID("LEF1","symbol","ensembl")
+getTF("ENSG00000138795",canonPathway) #TF for LEF1
 resultAnalysis <- ProcessTF("ENSG00000138795",canonPathway)
+resultAnalysis[[1]] #inhibitor
+resultAnalysis[[1]][1] #significant inhibitor that is found
+resultAnalysis[[1]][2] #not significant inhibitor
+resultAnalysis[2] #activator
+resultAnalysis[[2]][1] #significant activator that is found
+resultAnalysis[[2]][3] #regression result
+resultAnalysis[[2]][2] #not significant activator
 
+
+#run for all nodes
 allResult <- list()
 for(n in colnames(canonPathway)){
   allResult[[n]] <- ProcessTF(n,canonPathway)
